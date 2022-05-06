c0a51afb855c562aa85b05891f2fa532a0daa9dd
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-05-06 21:08:56 +0900

    classでインスタンス化

--- js/split.js
+++ js/split.js
@@ -1,10 +1,28 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelector('.animate-title');
-    const string = element.innerHTML.trim().split('');
+    const ta = new TxtAnimation('.animate-title');
+    const ta2 = new TxtAnimation('.animate-title-2');
+    setTimeout(() => {
+        ta.animate();
+        ta2.animate();
+    }, 1000);
 
-    element.innerHTML = string.reduce((ac, cur) => {
-        cur = cur.replace(/\s+/, '&nbsp;');
-        return `${ac}<span class="char">${cur}</span>`;
-    });
-}, "");
+});
+//クラスは「機能のまとまり」
+class TxtAnimation {
+    //クラスが初期化されるタイミングで呼ばれる関数
+    constructor(el) {
+        this.el = document.querySelector(el);
+        this.chars = el.innerHTML.trim().split('');
+        this.el.innerHTML = this._splitTxt();
+    }
+    _splitTxt() {
+        return this.chars.reduce((ac, cur) => {
+            cur = cur.replace(/\s+/, '&nbsp;');
+            return `${ac}<span class="char">${cur}</span>`;
+        }, "");
+    }
+    animate() {
+        this.el.classList.toggle('inview');
+    }
+}
