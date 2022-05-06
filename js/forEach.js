563df229fae0ee5e53e4f287d3216d7bfa39f55a
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-05-06 18:01:11 +0900

    edited forEach.

--- js/forEach.js
+++ js/forEach.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const array = [1, 2, 3, 4, 5, 6];
+
+//forEach()は第一引数にvalue、2にindex、3に配列（そのもの）が渡る
+array.forEach(function(value, i, ary) {
+    console.log(value, i, ary);
+});
+
+array.forEach(value => console.log(value)); //めちゃコンパクト
