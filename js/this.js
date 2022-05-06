40d74bef039238216e7ff733f7a416e6113256fd
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-05-06 22:14:38 +0900

    edit this.js

---
+++ js/this.js
@@ -0,0 +1,21 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    const el1 = new changeTxt("a@ge@a@ge@");
+    const el2 = new changeTxt("1@6@@5@166@888@");
+    console.log(el1);
+    console.log(el2);
+});
+
+console.log(this);
+
+class changeTxt {
+    constructor(el) {
+        this.el = el;
+        this._countTxt();
+    }
+    _countTxt() {
+        setTimeout(() => {
+            console.log(this.el.length);
+        }, 1000);
+    }
+}
