5f09a190ed8d8799dfee902388288d24a58214ac
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-05-07 22:11:15 +0900

    inherited class obj.

--- js/classObj.js
+++ js/classObj.js
@@ -5,7 +5,7 @@ const fruit = {
         console.log(this); //fruitを参照
         window.setTimeout(function() {
             console.log(this)
-        }.bind({ name: "durian" })); //thisの値を束縛する。アロー関数は暗黙的にbindしてくれるので注意。
+        }.bind({ name: "durian" }), 1000); //thisの値を束縛する。アロー関数は暗黙的にbindしてくれるので注意。
     }
 }
 
@@ -25,4 +25,16 @@ const fruit2 = new MyFruit();
 fruit.logStr();
 // fruit2.logStr();
 
-///////////////////////
+////////クラス継承
+class Myfruit2 extends MyFruit { //クラスを継承する
+    constructor() {
+        super(); //親のコンストラクタが呼び出される
+    }
+    logStr() {
+        console.log(this.weight * 2);
+    }
+}
+
+const fruit3 = new Myfruit2();
+fruit3.logStr();
+console.log(fruit3.name)
